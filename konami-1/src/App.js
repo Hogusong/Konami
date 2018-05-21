0944de61887f544d811c2c2fd57714a51f7221a9
Young <hogusong1@gmail.com>
2018-05-21 15:29:43 -0700

    modify App.js to add an input, button, and <Konami />

--- konami-1/src/App.js
+++ konami-1/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import Konami from './components/konami';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activated: false,
+      name: ''
+    };
+    this.toggleActivation = this.toggleActivation.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  toggleActivation() {
+    this.setState({ activated: !this.state.activated });
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome{this.state.name.length > 1 ? `, ${this.state.name}` : ``}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <input type="text" onChange={this.handleChange} placeholder="Your name" />
+
+        <div className="konami">
+          <button onClick={this.toggleActivation}>Toggle Konami Code</button>
+          {this.state.activated ? <Konami name={this.state.name} /> : <div>Konami Code: Deactivated</div> }
+        </div>
       </div>
     );
   }
